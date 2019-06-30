17008721cec3eec5712d7212611ee02f01a3d4a1
Matt <peysone@me.com>
2019-06-30 15:50:52 +0200

    probny commit

---
+++ src/main/java/com/sda/javagda22/Pjeski/domain/repository/SchroniskoRepository.java
@@ -0,0 +1,7 @@
+package com.sda.javagda22.Pjeski.domain.repository;
+
+import com.sda.javagda22.Pjeski.domain.model.Schronisko;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchroniskoRepository extends JpaRepository<Schronisko, Integer> {
+}
