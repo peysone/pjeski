6862313864ae42a361e2140e878ba02b2ffe2703
Matt <peysone@me.com>
2019-06-30 21:03:56 +0200

    komenty

--- src/main/java/com/sda/javagda22/Pjeski/domain/model/Animal.java
+++ src/main/java/com/sda/javagda22/Pjeski/domain/model/Animal.java
@@ -3,7 +3,10 @@ package com.sda.javagda22.Pjeski.domain.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
@@ -12,25 +15,16 @@ public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
-    private String identificationNumber;
-//    nr identyfikacyjny w schronisku - może też być nr chipa
-    private String description;
+
     private String name;
-    private Integer estimatedAge;
+    private Integer age;
 //    age orientacyjnie
     private String breed;
-    private Boolean isPurebred;
+    private Boolean lineage;
     private String dateSinceInShelter;
 //    month & year przyjęcia do schroniska
     private Boolean isAvalaibleToAdoption;
     private Boolean isAvailabraForAWalk;
 //    todo image
-    private AnimalType animalType;
-
-    // Szuca - dodana relacja do sheltera
-    @ManyToOne
-    private Shelter shelter;
-
-//foty dodajemy w JSP
 
 }
