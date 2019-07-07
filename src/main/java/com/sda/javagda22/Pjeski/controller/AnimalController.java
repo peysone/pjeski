6fbdfd86b937dcda6600e799532b1cde9fd328b6
mushy <97ploski@gmail.com>
2019-07-07 14:46:20 +0200

    Aktualizacja wyszukiwanie po miescie z listy

--- src/main/java/com/sda/javagda22/Pjeski/controller/AnimalController.java
+++ src/main/java/com/sda/javagda22/Pjeski/controller/AnimalController.java
@@ -3,6 +3,7 @@ package com.sda.javagda22.Pjeski.controller;
 import com.sda.javagda22.Pjeski.domain.model.FilterForm;
 import com.sda.javagda22.Pjeski.domain.model.animal.Animal;
 import com.sda.javagda22.Pjeski.service.AnimalService;
+import com.sda.javagda22.Pjeski.service.ShelterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ import java.util.Optional;
 @Slf4j
 public class AnimalController {
 
-    @Autowired
     private final AnimalService animalService;
+    private final ShelterService shelterService;
 
     // Szuca - od teraz animala dodajemy od razu do schroniska, ponieważ bez sensu jest dodawać go bez przypisania do schroniska
     //więc posłużyłam sie kodem z kliniki i stworzyłam coś takiego i tu tylko cerate jest zmienione
