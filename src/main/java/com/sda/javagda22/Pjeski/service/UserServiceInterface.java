ca895201e941d119597a94a2bf2e70103fdc9819
andrut <dodiedogg@poczta.onet.pl>
2019-07-13 10:25:34 +0200

    naprawione logowanie

--- src/main/java/com/sda/javagda22/Pjeski/service/UserServiceInterface.java
+++ src/main/java/com/sda/javagda22/Pjeski/service/UserServiceInterface.java
@@ -1,8 +1,9 @@
 package com.sda.javagda22.Pjeski.service;
 
 import com.sda.javagda22.Pjeski.domain.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserServiceInterface {
+public interface UserServiceInterface extends UserDetailsService {
 
     public User findUserByEmail(String email);
     public void saveUser(User user);
